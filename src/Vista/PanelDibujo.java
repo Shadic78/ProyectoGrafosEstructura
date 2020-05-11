c771ca5fed36eda784daffb7e9bf2525583bf8ee
Shadic78 <carlosjosue7889@gmail.com>
2020-05-11 10:05:58 -0500

    Ajustes ventana

--- src/Vista/PanelDibujo.java
+++ src/Vista/PanelDibujo.java
@@ -26,34 +26,33 @@ public class PanelDibujo extends JPanel {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
         configuraciones(g2d);
-        
+
         /*if(coordX.size() > 1) {
             g2d.setColor(new Color(255, 141, 141));
             g2d.setStroke(new BasicStroke(2));
             g2d.drawLine(coordX.get(0) + 22, coordY.get(0) + 22, coordX.get(1) + 22, coordY.get(1) + 22);
         }*/
-        
-        for(int i = 0; i < coordX.size(); i++) {
-            if(i > 0) {
+        for (int i = 0; i < coordX.size(); i++) {
+            if (i > 0) {
                 g2d.setColor(new Color(255, 141, 141));
                 g2d.setStroke(new BasicStroke(2));
-                g2d.drawLine(coordX.get(i - 1) + 22, coordY.get(i - 1) + 22, coordX.get(i) + 22, coordY.get(i) + 22);                
-            }            
+                g2d.drawLine(coordX.get(i - 1) + 22, coordY.get(i - 1) + 22, coordX.get(i) + 22, coordY.get(i) + 22);
+            }
         }
-        
+
         for (int i = 0; i < coordX.size(); i++) {
-            g2d.setColor(new Color(70, 206, 205));            
-            g2d.fillOval(coordX.get(i) - 4, coordY.get(i) - 4, 52, 52); 
+            g2d.setColor(new Color(70, 206, 205));
+            g2d.fillOval(coordX.get(i) - 4, coordY.get(i) - 4, 52, 52);
             g2d.setColor(new Color(81, 237, 236));
             g2d.fillOval(coordX.get(i), coordY.get(i), 44, 44);
         }
 
         g.dispose();
     }
-    
+
     private void configuraciones(Graphics2D g2d) {
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);        
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
     }
 
 }
